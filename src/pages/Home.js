e8f56ed49de7c6b080aa04bbd8d7304192bacfbb
Elakkiya <elakkiya321@gmail.com>
2024-05-28 19:59:04 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import Header from '../commonComponents/Header';
 import MainSection from '../commonComponents/MainSection';
 import Footer from '../commonComponents/Footer';
 
 const Home = () => {
+  const branchName = process.env.REACT_APP_BRANCH_NAME || 'Unknown';
 
   return (
     <>
+      <h2>Current Branch: {branchName} </h2>
       <Header />
       <MainSection />
       <Footer />
