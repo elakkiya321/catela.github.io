fcf2e77784c85d226ede86c9c8ff882c24ba6ab5
Elakkiya <elakkiya321@gmail.com>
2024-06-01 20:05:23 +0530

    fetching github details using Node

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Header from '../commonComponents/Header';
 import MainSection from '../commonComponents/MainSection';
 import Footer from '../commonComponents/Footer';
+import generatedGitInfo from '../generatedGitInfo.json'
 
 const Home = () => {
-  const branchName = process.env.REACT_APP_BRANCH_NAME || 'Unknown';
 
   return (
     <>
-      <h2>Current Branch: {branchName} </h2>
+      <h2>Current Branch: {generatedGitInfo.gitBranch} </h2>
       <Header />
       <MainSection />
       <Footer />
